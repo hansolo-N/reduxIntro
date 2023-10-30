5b323bd0a6195ec42c84e6de43dcd9d802151da8
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-10-30 10:32:09 +0200

    added middleware using thunks

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,16 +1,17 @@
+
+
 const initialStateAccount = {
     loan: 0,
     balance : 0 ,
-    loanPurpose: ""
+    loanPurpose: "",
+    isLoading: false
 }
-
-
 
 export default function  accountReducer( state = initialStateAccount,action){
 
     switch(action.type){
         case "account/deposit":
-            return {...state,balance:state.balance + action.payload}
+            return {...state,balance:state.balance + action.payload,isLoading:false}
 
             case "withdraw":
                 return {...state,balance:state.balance - action.payload}
@@ -25,14 +26,28 @@ export default function  accountReducer( state = initialStateAccount,action){
                     //later
                 if(state.loan>0)return state
                 return {...state,loan:0,loanPurpose:'',balance: state.balance - state.loan }
+            
+            case "account/convertingCurrency":
+                return {...state,isLoading:true}
 
         default:
           return state  
     }
 }
 
-function deposit(amount){
-    return {type:"account/deposit",payload:amount}
+function deposit(amount,currency){
+    if(currency === 'USD') return {type:"account/deposit",payload:amount}
+
+
+    return async function(dispatch,getState){
+        dispatch({type:'account/convertingCurrency'})
+        
+        const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`)
+        const data = await res.json()
+        const converted = data.rates.USD
+
+        dispatch({type:"account/deposit",payload:converted})
+    }
 } 
 function withdraw(amount){
     return {type:"withdraw",payload:amount}
